cf5169031590c3ad4e122c0511867c2b1f0e395f
Mfundo-debug <diditmfundo@gmail.com>
2023-08-22 14:58:57 +0200

    modified urls

--- seminar_registration_system/member_info_capture/urls.py
+++ seminar_registration_system/member_info_capture/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='member_info_capture/index.html'), name='member_info_capture'),
     path('member_info_capture/', views.member_info_capture, name='member_info_capture'),
+    path('find_user/', views.find_user, name='find_user'),
+    path('member_info_capture_success/', TemplateView.as_view(template_name='member_info_capture/member_info_capture_success.html'), name='member_info_capture_success'),
     
 ]
